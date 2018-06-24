6ee916ac5afbfc38c908a161d661f792585609ef
kentaro@chromebook <kentaro.linux@gmail.com>
2018-06-24 16:25:17 +0900

    Add token

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EFO"
+
+	// identifer + literal
+	IDENT = "IDENT" // add, foobar, x, y, ....
+	INT = "INT"
+
+	// calcurator
+	ASSIGN = "="
+	PLUS = "+"
+
+	// delimiter
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRANCE = "{"
+	RBRANCE = "}"
+
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
